4fc9eb7c232429e3cf62fcccf199c4f962d2e986
yoonhyunsun <hsyoon@i2max.co.kr>
2022-03-16 10:29:11 +0900

    test class

--- force-app/main/default/lwc/partsInventoryAdjustment/partsInventoryAdjustment.js
+++ force-app/main/default/lwc/partsInventoryAdjustment/partsInventoryAdjustment.js
@@ -3,12 +3,16 @@ import { getRecord, updateRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import createTrans from '@salesforce/apex/transactionController.createTrans';
 import Quantity__c_FIELD from '@salesforce/schema/Parts_Inventory__c.Quantity__c';
-
+import Updated_Quantity__c_FIELD from '@salesforce/schema/Parts_Inventory_Transaction__c.Updated_Quantity__c';
+import Confirmed_Quantity__c_FIELD from '@salesforce/schema/Parts_Inventory_Transaction__c.Confirmed_Quantity__c';
+import Transaction_Date__c_FIELD from '@salesforce/schema/Parts_Inventory_Transaction__c.Transaction_Date__c';
+import Description__c_FIELD from '@salesforce/schema/Parts_Inventory_Transaction__c.Description__c';
+import Status__c_FIELD from '@salesforce/schema/Parts_Inventory_Transaction__c.Status__c';
+import Parts_Inventory__c_FIELD from '@salesforce/schema/Parts_Inventory_Transaction__c.Parts_Inventory__c';
 
 
 export default class createRecordForm extends LightningElement {
     @api recordId;
-    @wire(getRecord, { recordId: "$recordId", fields: [Quantity__c_FIELD] })
     onChangeUpdatedQuantity;
     updatedQuantity;
     onChangeConfirmedQuantity;
@@ -50,14 +54,16 @@ export default class createRecordForm extends LightningElement {
     }
     
     handleClick(){
-        createTrans({
-            UQuantity: this.updatedQuantity,
-            CQuantity: this.confirmedQuantity,
-            TDate: this.transactionDate,
-            Status: this.status,
-            Description: this.description,
-            PInventory: this.recordId
-        }).then(result=>{
+        const recordInput = {
+                [Updated_Quantity__c_FIELD.fieldApiName] : this.updatedQuantity,
+                [Confirmed_Quantity__c_FIELD.fieldApiName] : this.confirmedQuantity,
+                [Transaction_Date__c_FIELD.fieldApiName] : this.transactionDate,
+                [Status__c_FIELD.fieldApiName] : this.status,
+                [Description__c_FIELD.fieldApiName] : this.description,
+                [Parts_Inventory__c_FIELD.fieldApiName] : this.recordId
+        };
+        createTrans({ trans : recordInput })
+        .then(result=>{
             this.dispatchEvent(new ShowToastEvent({
                 title: "Transaction created",
                 variant: "success"
@@ -73,7 +79,8 @@ export default class createRecordForm extends LightningElement {
             console.log(this.status);
         }).catch(error=>{
             this.dispatchEvent(new ShowToastEvent({
-                title: "Error",
+                title: 'Error',
+                message: error.body.message,
                 variant: "error"
             }));
             this.message = 'Error';
@@ -81,3 +88,5 @@ export default class createRecordForm extends LightningElement {
     }
 }
 
+// 클래스에서 변수 말고 object로 넣기.
+// error 는 object.
--- force-app/main/default/triggers/ProductTrigger.trigger
+++ force-app/main/default/triggers/ProductTrigger.trigger
@@ -1,35 +1,32 @@
 trigger ProductTrigger on Product2 (before insert, before update) {
     
-    Map<String, Id> productMap = new Map<String, Id>();
-    for (Product2 p : Trigger.new) {
-    productMap.put(p.AMOSSAC__c, p.Id);
-    
-    Parts_Market_Factor__c[] fList = [SELECT Id, Market_Factor__c, SAC__c FROM Parts_Market_Factor__c WHERE SAC__c IN :productMap.keySet()];
+    Set<String> productSac = new Set<String>();
     Set<String> marketFactorSac = new Set<String>();
-    for(Parts_Market_Factor__c f : fList){
-        marketFactorSac.add(f.SAC__c);
+
+    Parts_Market_Factor__c[] fList = [SELECT Id, Market_Factor__c, SAC__c FROM Parts_Market_Factor__c LIMIT 2000];
+    List<Parts_Market_Factor__c> newFList = new List<Parts_Market_Factor__c>();
+    for(Parts_Market_Factor__c mf : fList){
+        marketFactorSac.add(mf.SAC__c);
+        newFList.add(mf);
     }
-            
+
+    for (Product2 p : Trigger.new) {
+        productSac.add(p.AMOSSAC__c);
+    
     if(!marketFactorSac.contains(p.AMOSSAC__c)){
-        Parts_Market_Factor__c marketFactor = new Parts_Market_Factor__c();
-        for(String sac : productMap.keySet()){
-            marketFactor.SAC__c = sac;
-            marketFactor.Market_Factor__c = 1;
-            p.NRRP__c = p.MSRP__c * marketFactor.Market_Factor__c;
-            
+        for(Parts_Market_Factor__c marketFactor : newFList){
+            for(String sac : productSac){
+                marketFactor.SAC__c = sac;
+                marketFactor.Market_Factor__c = 1;
+                p.NRRP__c = p.MSRP__c * marketFactor.Market_Factor__c;
+            }
+            System.debug(marketFactor);
+            insert marketFactor;
         }
-        System.debug(marketFactor);
-        insert marketFactor;
     }else{
-        for(Parts_Market_Factor__c f : fList){
-        p.NRRP__c = p.MSRP__c * f.Market_Factor__c;
+        for(Parts_Market_Factor__c marketFactor : newFList){
+        p.NRRP__c = p.MSRP__c * marketFactor.Market_Factor__c;
         }
     }
     }
 }
-
-
-
-
-    
-    
